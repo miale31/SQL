7eeb844a3b4e6a1437b0c345c07cbebbb8ac3ef8
miale31 <161149110+miale31@users.noreply.github.com>
2024-02-24 23:49:30 +0700

    Update Practice_1.sql

--- Practice_1.sql
+++ Practice_1.sql
@@ -14,6 +14,11 @@ select distinct city, state from station;
 select city from station
 where city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%';
 
+-- Excercise 5
+select distinct city from station
+where city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u';
+
+
 
 
 
