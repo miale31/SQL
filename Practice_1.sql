84ccf1f36e3b9f021a581d81198ac4793cc9ba70
miale31 <161149110+miale31@users.noreply.github.com>
2024-02-24 23:37:31 +0700

    Update Practice_1.sql

--- Practice_1.sql
+++ Practice_1.sql
@@ -3,4 +3,18 @@ select name from city
 where countrycode = 'USA'
 and population > 120000;
 
+-- Excercise 2
+select * from city
+where countrycode = 'JPN';
+
+-- Excercise 3
+select distinct city, state from station;
+
+-- Exercise 4
+select city from station
+where city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%';
+
+
+
+
 
