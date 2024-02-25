216af674fc7f1c1157c5f3429af3a50ac66a7661
miale31 <161149110+miale31@users.noreply.github.com>
2024-02-25 23:02:08 +0700

    Update Practice_1.sql

--- Practice_1.sql
+++ Practice_1.sql
@@ -26,6 +26,49 @@ where not (city like 'a%' or city like 'e%' or city like 'i%' or city like 'o%' 
 select name from Employee
 order by name;
 
+-- Excercise 8
+select name from Employee
+where salary > 2000
+and months < 10
+order by employee_id;
+
+-- Excercise 9
+select product_id from Products
+where low_fats = 'Y' and recyclable = 'Y';
+
+-- Excercise 10
+select name from Customer
+where not referee_id = 2 or referee_id is null;
+
+-- Excercise 11
+select name, population, area from World
+where area >= 3000000
+or population >= 25000000;
+
+-- Excercise 12
+select distinct author_id as id from Views
+where author_id = viewer_id
+order by author_id;
+
+-- Excercise 13
+SELECT part, assembly_step FROM parts_assembly
+where finish_date is null;
+
+-- Excercise 14
+select * from lyft_drivers
+where yearly_salary <= 30000 or yearly_salary >= 70000;
+
+-- Excercise 15
+select * from uber_advertising
+where year = 2019
+and money_spent > 100000;
+
+
+
+
+
+
+
 
 
 
