9a624838ba0771c95c01a6396928b4da57f384af
miale31 <161149110+miale31@users.noreply.github.com>
2024-02-28 14:55:45 +0700

    Update Practice_2.sql

--- Practice_2.sql
+++ Practice_2.sql
@@ -1,4 +1,28 @@
 -- Excercise 1
 select distinct city from station
 where id%2=0;
+-- Excercise 2
+select count(city) - count(distinct city) from station;
+
+-- Excercise 3
+
+-- Excercise 4
+SELECT round(cast(sum (item_count * order_occurrences)/ sum (order_occurrences) as decimal),1) as mean FROM items_per_order;
+
+-- Excercise 5
+SELECT candidate_id FROM candidates
+WHERE skill IN ('Python','Tableau','PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(skill) = 3;
+
+-- Excercise 6
+-- Excercise 7
+-- Excercise 8
+-- Excercise 9
+-- Excercise 10
+
+
+
+
+
 
