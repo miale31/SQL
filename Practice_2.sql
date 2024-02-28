d8cb4397ca8c20f1e42e4f67ea40aa0e04f9f3b8
miale31 <161149110+miale31@users.noreply.github.com>
2024-02-28 21:06:07 +0700

    Update Practice_2.sql

--- Practice_2.sql
+++ Practice_2.sql
@@ -17,9 +17,42 @@ HAVING COUNT(skill) = 3;
 
 -- Excercise 6
 -- Excercise 7
+
 -- Excercise 8
+SELECT
+  manufacturer,
+  COUNT(drug) AS number_of_drugs, 
+  ABS(SUM(total_sales - cogs)) as total_loss
+FROM pharmacy_sales
+WHERE total_sales - cogs <=0
+GROUP BY manufacturer
+ORDER BY total_loss DESC;
+
 -- Excercise 9
+SELECT * FROM CINEMA
+WHERE NOT ID%2 = 0
+AND DESCRIPTION <> 'BORING'
+ORDER BY RATING DESC;
+
 -- Excercise 10
+SELECT 
+TEACHER_ID,
+COUNT(DISTINCT SUBJECT_ID) AS CNT 
+FROM TEACHER
+GROUP BY TEACHER_ID;
+
+-- Excercise 11
+SELECT
+USER_ID,
+COUNT(FOLLOWER_ID) AS FOLLOWERS_COUNT
+FROM FOLLOWERS
+GROUP BY USER_ID;
+
+--Excercise 12
+SELECT CLASS FROM COURSES
+HAVING COUNT(CLASS)>=5;
+
+
 
 
 
