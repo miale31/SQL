5eb8ff30aa2b201e5632a48aa285978b1b790e36
miale31 <161149110+miale31@users.noreply.github.com>
2024-03-09 23:17:00 +0700

    Update Practice_5.sql

--- Practice_5.sql
+++ Practice_5.sql
@@ -6,12 +6,23 @@ group by country.continent;
 
 --Excercise 2
 SELECT
-round((sum(case when texts.signup_action = 'Confirmed' then 1 end))/
-(count(texts.signup_action)),2) as confirm_rate
+round((sum(case when texts.signup_action = 'Confirmed' then 1 else 0 end))/
+count(texts.signup_action),2) as confirm_rate
 FROM emails
 left join texts on emails.email_id = texts.email_id;
 
 --Excercise 3
+SELECT
+  customer_contracts.customer_id
+FROM 
+  customer_contracts
+LEFT JOIN
+  products ON customer_contracts.product_id = products.product_id
+group by 
+  customer_contracts.customer_id
+having 
+  count(DISTINCT products.product_category) = 3;
+
 --Excercise 4
 --Excercise 5
 --Excercise 6
