5d6983094ad0bb02c341dae926b8ee48f4e595d7
miale31 <161149110+miale31@users.noreply.github.com>
2024-03-09 22:50:01 +0700

    Update Practice_5.sql

--- Practice_5.sql
+++ Practice_5.sql
@@ -5,6 +5,12 @@ Inner join city on city.countrycode = country.code
 group by country.continent;
 
 --Excercise 2
+SELECT
+round((sum(case when texts.signup_action = 'Confirmed' then 1 end))/
+(count(texts.signup_action)),2) as confirm_rate
+FROM emails
+left join texts on emails.email_id = texts.email_id;
+
 --Excercise 3
 --Excercise 4
 --Excercise 5
